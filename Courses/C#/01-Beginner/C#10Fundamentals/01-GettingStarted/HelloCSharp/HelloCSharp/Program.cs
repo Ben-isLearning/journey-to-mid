856b38fb8355007ffafdf87eb2bb1a300102a116
Ben Wingrove <ben.wingrove@inflosoftware.com>
2023-06-05 19:24:41 +0100

    accepting User Input in CLI

--- Courses/C#/01-Beginner/C#10Fundamentals/01-GettingStarted/HelloCSharp/HelloCSharp/Program.cs
+++ Courses/C#/01-Beginner/C#10Fundamentals/01-GettingStarted/HelloCSharp/HelloCSharp/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+//Lesson01
 Console.WriteLine("Hello, World!");
+
+//Lesson02
+Console.WriteLine("What is your name?");
+string userInput = Console.ReadLine();
+Console.WriteLine($"Hello {userInput}!");
