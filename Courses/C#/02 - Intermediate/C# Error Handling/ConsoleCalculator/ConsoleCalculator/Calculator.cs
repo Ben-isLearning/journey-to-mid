1fb36b3738060abc23a5f154cbfaeb39156045c3
Ben Wingrove <ben.wingrove@inflosoftware.com>
2023-06-20 12:11:43 +0100

    Started Error Handling

---
+++ Courses/C#/02 - Intermediate/C# Error Handling/ConsoleCalculator/ConsoleCalculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ConsoleCalculator;
+
+public class Calculator
+{
+    public int Calculate(int num1, int num2, string operation)
+    {
+        if (operation == "/")
+        {
+            return Divide(num1, num2);
+        }
+        else {
+            Console.WriteLine("Unknown Operation.");
+            return 0;
+        }
+    }
+
+    private int Divide(int num1, int num2) => num1 / num2; 
+}
---
+++ Courses/C#/02 - Intermediate/C# Error Handling/ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleCalculator;
+using static System.Console;
+
+WriteLine("Enter first number"); 
+int number1 = int.Parse(Console.ReadLine()!);
+
+WriteLine("Enter second number");
+int number2 = int.Parse(Console.ReadLine()!);
+
+WriteLine("Enter operation");
+string operation = ReadLine()!.ToUpperInvariant();
+
+var calculator = new Calculator();
+var result = calculator.Calculate(number1, number2, operation);
+
+DisplayResult(result);
+
+WriteLine("\nPress enter to exit.");
+ReadLine();
+
+
+static void DisplayResult(int result) => WriteLine($"Result is: {result}");
