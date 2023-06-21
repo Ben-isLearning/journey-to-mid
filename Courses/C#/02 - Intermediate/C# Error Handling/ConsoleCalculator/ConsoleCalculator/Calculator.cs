a298a2a7d27733e4674708596f08bbb9cd5986ea
Ben Wingrove <ben.wingrove@inflosoftware.com>
2023-06-21 15:13:10 +0100

    Continuing with Error Handling

--- Courses/C#/02 - Intermediate/C# Error Handling/ConsoleCalculator/ConsoleCalculator/Calculator.cs
+++ Courses/C#/02 - Intermediate/C# Error Handling/ConsoleCalculator/ConsoleCalculator/Calculator.cs
@@ -6,11 +6,26 @@ public class Calculator
 {
     public int Calculate(int num1, int num2, string operation)
     {
-        if (operation == "/")
+       
+        string nonNullOperation =
+            operation ?? throw new ArgumentNullException(nameof(operation));
+
+        if (nonNullOperation == "/")
         {
-            return Divide(num1, num2);
+            try
+            {
+                return Divide(num1, num2);
+            } 
+            catch (DivideByZeroException ex) 
+            {
+                Console.WriteLine("...Logging...");
+                //Log.Error(ex);
+                //throw; 
+                throw new ArithmeticException("An error occurred during calculation.", ex);
+            }
         }
-        else {
+        else
+        {
             throw new ArgumentOutOfRangeException(nameof(operation), "The mathematical operator is not supported.");
 
             //Console.WriteLine("Unknown Operation.");
@@ -18,5 +33,5 @@ public class Calculator
         }
     }
 
-    private int Divide(int num1, int num2) => num1 / num2; 
+    private int Divide(int num1, int num2) => num1 / num2;
 }
--- Courses/C#/02 - Intermediate/C# Error Handling/ConsoleCalculator/ConsoleCalculator/Program.cs
+++ Courses/C#/02 - Intermediate/C# Error Handling/ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -1,6 +1,9 @@
 ﻿using ConsoleCalculator;
 using static System.Console;
 
+AppDomain currentAppDomain = AppDomain.CurrentDomain;
+currentAppDomain.UnhandledException += new UnhandledExceptionEventHandler(HandleException);
+
 WriteLine("Enter first number");
 int number1 = int.Parse(Console.ReadLine()!);
 
@@ -17,12 +20,39 @@ try
 
     DisplayResult(result);
 }
+catch (ArgumentNullException ex) when (ex.ParamName == "operation")
+{
+    //Exception Filter is more specific than unfiltered
+    ///Log.Error(ex);
+    WriteLine($"Operation was not provided. {ex}");
+}
+catch (ArgumentNullException ex)
+{
+    //Log.Error(ex);
+    WriteLine($"An argument was null. {ex}");
+}
+catch (ArgumentOutOfRangeException ex)
+{
+    //Log.Error(ex);
+    WriteLine($"Operation is not supported. {ex}");
+}
 catch (Exception ex)
 {
     WriteLine($"Sorry, something went wrong. {ex}");
 }
+finally
+{
+    //Dispose here 
+    WriteLine($"Finally...");
+}
+
 WriteLine("\nPress enter to exit.");
 ReadLine();
 
 
 static void DisplayResult(int result) => WriteLine($"Result is: {result}");
+
+static void HandleException(object sender, UnhandledExceptionEventArgs e)
+{
+    WriteLine($"Sorry there was a problem an we need to close. Details: {e.ExceptionObject}");
+}
